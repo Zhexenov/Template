9f3d40420c285e2494341dc0873eea3e66c51250
Zhexenov <fitsam01@gmail.com>
2019-08-07 17:41:12 +0600

    Update ___FILEBASENAME___DataManager.swift

--- DataManager.xctemplate/___FILEBASENAME___DataManager.swift
+++ DataManager.xctemplate/___FILEBASENAME___DataManager.swift
@@ -5,14 +5,21 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ Jex. All rights reserved.
 
+
 import Moya
 
 
-public class ___VARIABLE_productName:identifier___DataManager: I___VARIABLE_productName:identifier___DataManager {
-    public var handler: I___VARIABLE_productName:identifier___DataManagerOutput?
+class ___VARIABLE_productName:identifier___DataManager: I___VARIABLE_productName:identifier___DataManager {
     
-    private var provider: MoyaProvider<___VARIABLE_productName:identifier___Api> {
-        return MoyaProvider<___VARIABLE_productName:identifier___Api>()
-    }
+    weak var handler: I___VARIABLE_productName:identifier___DataManagerOutput?
     
+    private var provider: MoyaProvider<___VARIABLE_productName:identifier___Api>
+    
+//     private var provider: MoyaProvider<___VARIABLE_productName:identifier___Api> {
+//         return MoyaProvider<___VARIABLE_productName:identifier___Api>()
+//     }
+    
+    init(sessionManager: ISessionManager) {
+        provider = API.provider(sessionManager: sessionManager)
+    }   
 }
