8867446c83bbcfd71a640775d4ec971440cff4bc
Zhexenov <fitsam01@gmail.com>
2019-08-07 17:13:43 +0600

    Update ___FILEBASENAME___Protocols.swift

--- Module.xctemplate/___FILEBASENAME___Protocols.swift
+++ Module.xctemplate/___FILEBASENAME___Protocols.swift
@@ -15,18 +15,18 @@ public protocol I___VARIABLE_productName:identifier___Wireframe: class {
     
 }
 
+
 /* Interactor */
+public protocol I___VARIABLE_productName: identifier___Interactor: IBaseInteractor {
+    var presenter: I___VARIABLE_productName:identifier___InteractorOutput? { get set }
+    var dataManager: I___VARIABLE_productName:identifier___DataManager? { get set }
+
+}
+
 public protocol I___VARIABLE_productName: identifier___InteractorOutput: IBaseInteractorOutput {
     
 }
 
-public protocol I___VARIABLE_productName: identifier___Interactor: IBaseInteractor {
-    
-    var presenter: I___VARIABLE_productName:identifier___InteractorOutput? { get set }
-    
-    var dataManager: I___VARIABLE_productName:identifier___DataManager? { get set }
-
-}
 
 /* Presenter */
 public protocol I___VARIABLE_productName:identifier___Presenter: IBasePresenter {
@@ -34,6 +34,7 @@ public protocol I___VARIABLE_productName:identifier___Presenter: IBasePresenter 
     var parameters: [String: Any]? { get set }
     
 }
+
 
 /* View */
 public protocol I___VARIABLE_productName:identifier___View: IBaseView {
