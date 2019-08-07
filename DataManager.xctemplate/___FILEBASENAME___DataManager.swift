4ebdc8ccbf72aa216769125a68c507335ee23722
Zhexenov <fitsam01@gmail.com>
2019-08-07 17:27:32 +0600

    Update ___FILEBASENAME___DataManager.swift

--- DataManager.xctemplate/___FILEBASENAME___DataManager.swift
+++ DataManager.xctemplate/___FILEBASENAME___DataManager.swift
@@ -5,17 +5,14 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ Jex. All rights reserved.
 
-import Foundation
 import Moya
+
 
 public class ___VARIABLE_productName:identifier___DataManager: I___VARIABLE_productName:identifier___DataManager {
     public var handler: I___VARIABLE_productName:identifier___DataManagerOutput?
     
     private var provider: MoyaProvider<___VARIABLE_productName:identifier___Api> {
         return MoyaProvider<___VARIABLE_productName:identifier___Api>()
-        
-        // with verbose logs
-//        return MoyaProvider<___VARIABLE_productName:identifier___Api>(plugins: [NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter)])
     }
     
 }
