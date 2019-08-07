753e26c91d5f6758c20862f97b0d41bab8479b80
Zhexenov <fitsam01@gmail.com>
2019-08-07 17:26:19 +0600

    Update ___FILEBASENAME___Api.swift

--- DataManager.xctemplate/___FILEBASENAME___Api.swift
+++ DataManager.xctemplate/___FILEBASENAME___Api.swift
@@ -5,7 +5,6 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ Jex. All rights reserved.
 
-import Foundation
 import Moya
 
 
@@ -17,7 +16,7 @@ public enum ___VARIABLE_productName:identifier___Api {
 extension ___VARIABLE_productName:identifier___Api: TargetType {
     
     public var baseURL: URL {
-        guard let url = URL(string: "baseUrl") else { fatalError("Invalid base url") }
+        guard let url = URL(string: baseUrl) else { fatalError("Invalid base url") }
         return url
     }
     
