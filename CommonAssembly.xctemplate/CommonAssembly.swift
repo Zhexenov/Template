67ac849844395e5a6f1f316a6c8057753456838d
Zhexenov <fitsam01@gmail.com>
2019-08-07 17:35:23 +0600

    Update CommonAssembly.swift

--- CommonAssembly.xctemplate/CommonAssembly.swift
+++ CommonAssembly.xctemplate/CommonAssembly.swift
@@ -5,8 +5,8 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ Jex. All rights reserved.
 
-import Foundation
 import Swinject
+
 
 public class CommonAssembly: Assembly {
 	
@@ -14,13 +14,5 @@ public class CommonAssembly: Assembly {
 		container.register(IAppRouter.self) { _ in
 			AppRouter.shared
 		}
-
-		/*
-		 | Register your manager here, ex :
-		 |
-		 | container.register(ISampleManager.self) {(r) in
-		 |	    return SampleManager()
-		 | }
-		 */
 	}
 }
